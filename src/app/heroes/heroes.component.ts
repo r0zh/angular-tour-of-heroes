7b620bc216262d97140576419499b143f2f914bf
r0zh <r0zh017@proton.me>
2024-01-08 15:45:36 +0000

    create hero interface

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero = 'Lucio';
+  hero: Hero = {
+    id: 1,
+    name: 'Lucio',
+  };
 }
